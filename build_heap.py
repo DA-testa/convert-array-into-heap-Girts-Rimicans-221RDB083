f606864b5f1da0f7207c3b846e7fe68493f1f4d3
Girts-Rimicans-221RDB083 <124580570+Girts-Rimicans-221RDB083@users.noreply.github.com>
2023-03-19 17:01:44 +0000

    optimize

--- build_heap.py
+++ build_heap.py
@@ -15,49 +15,38 @@ def parse_input_file(file_name):
     # print(data)
     return n, data
 
-def create_heap(data, n, i, swaps, swap_amount):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-
-    if left < n and data[i] < data[left]:
-        largest = left
-
-    if right < n and data[largest] < data[right]:
-        largest = right
-
-    if largest != i:
-        swap_amount = swap_amount + 1
-        swaps.append([data[i], data[largest]])
-
-        data[i], data[largest] = data[largest], data[i]
-        create_heap(data, n, largest, swaps, swap_amount)
-
-    return swaps, swap_amount
-
-def build_heap(data, swaps, swap_amount):
+def build_heap(data):
     n = len(data)
+    swaps = []
+    for i in range(n // 2, -1, -1):
+        top = i
+        while True:
+            left = 2 * top + 1
+            right = 2 * top + 2
+            if left < n and data[left] < data[top]:
+                top = left
+            if right < n and data[right] < data[top]:
+                top = right
+            if i != top:
+                data[i], data[top] = data[top], data[i]
+                swaps.append((i, top))
+                i = top
+            else:
+                break
 
-    for i in range(n//2, -1, -1):
-        swaps, swap_amount = create_heap(data, n, i, swaps, swap_amount)
-  
-    for i in range(n-1, 0, -1):
-        data[i], data[0] = data[0], data[i]
-        swaps, swap_amount = create_heap(data, i, 0, swaps, swap_amount)
-
-    return swaps, swap_amount
+    return swaps
 
 def main():
-    n, swap_amount = 0, 0
-    input_data, data, swaps = [], [], []
+    n = 0
+    input_data = []
 
     try:
         key = input().strip()
         # print(key)
         if (key.upper() == "I"):
             n, input_data = parse_input_user()
-        elif (key.upper() == "F"):
-            file_name = input().strip()
+        eleftif (key.upper() == "F"):
+            filefte_name = input().strip()
             if (file_name.lower() == "a"):
                 pass
             n, input_data = parse_input_file("tests/" + file_name)
@@ -65,28 +54,16 @@ def main():
         pass
 
     # checks if lenght of data is the same as the said lenght
-    data = input_data.copy()
-    assert len(data) == n
+    assert len(input_data) == n
     # calls function to assess the data 
     # and give back all swaps
-    swaps, swap_amount = build_heap(data, swaps, swap_amount)
-    if input_data == data:
-        print(0)
-        return
-
-    if swaps:
-        swaps.pop()
-    else:
-        print(swap_amount)
-        return
+    swaps = build_heap(input_data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-    print(swap_amount)
-    if swap_amount != 0 and swap_amount < 1000:
-        # output all swaps
-        for i in swaps:
-            print(i[0], i[1])
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
 
 
 if __name__ == "__main__":
